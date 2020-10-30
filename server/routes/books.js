64efa0d4da09f66d875040b2caa3a6f383403382
nivethaa8 <nivethaa.321@gmail.com>
2020-10-30 23:44:48 +0530

    added other routes

--- server/routes/books.js
+++ server/routes/books.js
@@ -35,24 +35,86 @@ router.get('/add', (req, res, next) => {
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', async (req, res, next) => {
 
-
+    try{
+        let newBook = new book({
+            Title: req.body.title,
+            Description: '',
+            Author: req.body.author,
+            Price: req.body.price,
+            Genre: req.body.genre
+        });
+        let result = await newBook.save();
+        res.redirect('/books');
+    }catch (e) {
+        res.status(501).send('Something went wrong');
+    }
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id',async (req, res, next) => {
+    if(!req.params.id){
+        res.status(400).send('Object id is not provided');
+    }
+    try{
+        let id = req.params.id;
+        let result = await book.findById(id);
 
+        res.render('books/details',{
+            title: 'Books',
+            type: 'edit',
+            books: result
+        });
+    }catch (e) {
+        res.status(404).send('no books found with given id');
+    }
 });
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', async (req, res, next) => {
 
+    if(req.params.id){
+       let id = req.params.id;
+        try{
+            let resultBook = await book.findById(id);
+            if(!resultBook){
+                res.status(404).send('Object not found with given id');
+            }
 
+            resultBook.Title = req.body.title;
+            resultBook.Description = '';
+            resultBook.Author = req.body.author;
+            resultBook.Price = req.body.price;
+            resultBook.Genre = req.body.genre;
+
+            let result = await  resultBook.save();
+            res.redirect('/books');
+        }catch (e) {
+            res.status(501).send('Something went wrong');
+        }
+    }else{
+        res.status(400).send('Id is not provoded');
+    }
 
 });
 
 // GET - process the delete by user id
 router.get('/delete/:id', async (req, res, next) => {
+    if(req.params.id){
+        let id = req.params.id;
+        try{
+            let resultBook = await book.findById(id);
+            if(!resultBook){
+                res.status(404).send('Object not found with given id');
+            }
 
+            let result = await book.findByIdAndDelete(id);
+            res.redirect('/books');
+        }catch (e) {
+            res.status(501).send('Something went wrong');
+        }
+    }else{
+        res.status(400).send('Id is not provoded');
+    }
 });
 
 
